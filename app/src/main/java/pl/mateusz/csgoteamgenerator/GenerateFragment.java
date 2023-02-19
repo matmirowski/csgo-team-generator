2818a6825895ad7da9b58799dd1edd2de190e1c6
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2023-02-19 12:43:50 +0100

    feat: Add switch to choose image source in PlayerAdd Fragment

--- app/src/main/java/pl/mateusz/csgoteamgenerator/GenerateFragment.java
+++ app/src/main/java/pl/mateusz/csgoteamgenerator/GenerateFragment.java
@@ -41,6 +41,7 @@ import java.net.URL;
 import java.util.Random;
 
 public class GenerateFragment extends Fragment {
+    //TODO round button corners?
     private static final String liquipediaURL = "https://liquipedia.net/counterstrike/";
     private final ImageView[] playerImageViews = new ImageView[5];
     private final TextView[] playerTextViews = new TextView[5];
