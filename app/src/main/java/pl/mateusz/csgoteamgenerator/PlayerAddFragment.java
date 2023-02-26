7bdfad440bd8d74728e82f98f439984dcd66a6b6
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2023-02-26 18:42:33 +0100

    test: Create tests class for PlayerAddFragment

--- app/src/main/java/pl/mateusz/csgoteamgenerator/PlayerAddFragment.java
+++ app/src/main/java/pl/mateusz/csgoteamgenerator/PlayerAddFragment.java
@@ -14,6 +14,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
